188b4c4b5c98235b2c4f308a4f356fa6e1d74095
Kristen Chellis and Matt Dowdney <kristen@>
2018-03-28 15:39:42 -0700

    add function to convert from any base 2-16 to decimal

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,61 @@
-function baseCheck(string){
-  if (/[^0123456789abcdef]/.test(string)) {
+function baseCheck(string, base){
+  if (/[^0123456789abcdef]/.test(string) && base===16) {
+    return false;
+  }else if (/[^0123456789abcde]/.test(string) && base===15) {
+    return false;
+  }else if (/[^0123456789abcd]/.test(string) && base===14) {
+    return false;
+  }else if (/[^0123456789abc]/.test(string) && base===13) {
+    return false;
+  }else if (/[^0123456789ab]/.test(string) && base===12) {
+    return false;
+  }else if (/[^0123456789a]/.test(string) && base===11) {
+    return false;
+  }else if (/[^0123456789]/.test(string) && base===10) {
+    return false;
+  }else if (/[^012345678]/.test(string) && base===9) {
+    return false;
+  }else if (/[^01234567]/.test(string) && base===8) {
+    return false;
+  }else if (/[^0123456]/.test(string) && base===7) {
+    return false;
+  }else if (/[^012345]/.test(string) && base===6) {
+    return false;
+  }else if (/[^01234]/.test(string) && base===5) {
+    return false;
+  }else if (/[^0123]/.test(string) && base===4) {
+    return false;
+  }else if (/[^012]/.test(string) && base===3) {
+    return false;
+  }else if (/[^01]/.test(string) && base===2) {
     return false;
   }else {
     return true;
   }
 }
-function toDecimal(string){
+function toDecimal(string, base){
   var reverse = "";
   for (var i = (string.length-1); i >= 0; i--) {
     reverse += string[i];
   }
   var array= reverse.split("");
-  console.log(array);
   var decimal = 0;
   for (var i = 0; i < array.length; i++) {
     if (array[i]=== "a") {
-      decimal += 10 * Math.pow(16, i);
+      decimal += 10 * Math.pow(base, i);
       console.log(decimal);
     }else if (array[i] ==="b") {
-      decimal += 11 * Math.pow(16, i);
+      decimal += 11 * Math.pow(base, i);
     }else if (array[i] ==="c") {
-      decimal += 12 * Math.pow(16, i);
+      decimal += 12 * Math.pow(base, i);
     }else if (array[i] ==="d") {
-      decimal += 13 * Math.pow(16, i);
+      decimal += 13 * Math.pow(base, i);
     }else if (array[i] ==="e") {
-      decimal += 14 * Math.pow(16, i);
+      decimal += 14 * Math.pow(base, i);
     }else if (array[i] ==="f") {
-      decimal += 15 * Math.pow(16, i);
+      decimal += 15 * Math.pow(base, i);
     }else {
-      decimal += (parseInt(array[i]) * Math.pow(16, i));
+      decimal += (parseInt(array[i]) * Math.pow(base, i));
     }
   }
   return decimal;
@@ -38,11 +65,12 @@ $(document).ready(function(){
   $(".form1").submit(function(event){
     event.preventDefault();
     var input = $("#binary").val();
-    if(baseCheck(input)) {
-      var result = toDecimal(input);
-      $("#decimal").text(input + " is " + result + " in decimal.");
+    var base = parseInt($("#base").val());
+    if(baseCheck(input, base)) {
+      var result = toDecimal(input, base);
+      $("#decimal").text(input + " in base " + base + " is " + result + " in decimal.");
     }else{
-      alert("Please enter a hexadecimal number only");
+      alert("The number entered is not in the correct base");
     }
   });
 });
